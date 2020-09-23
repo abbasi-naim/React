48e8df1b64e6030aec537f58baa6f9d27b61a874
abbasi-naim <abbasi.naim@gmail.com>
2020-09-24 01:21:48 +0330

    Add ReactDOM and React to index.js and App.js files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom'
+import React from 'react'
 import './Style.css';
+
+const name = "Naim And Rana"
+const msg = <p>Welcome to {name} React Site!! </p>
+
+ReactDOM.render(
+    msg,
+    document.getElementById("root"));
+
+    let counter = 0 
+    function show() {
+        counter++;
+    const mesg = <p>{counter}</p>;
+    ReactDOM.render(
+        mesg,
+        document.getElementById("root")
+    );
+        
+    }
+setInterval(show,2000);
+
