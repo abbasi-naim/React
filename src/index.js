1d57c4b9ae34ebf0ca81ee022fa09cbf739dead9
abbasi-naim <abbasi.naim@gmail.com>
2020-09-24 13:23:29 +0330

    Components - functional and class

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const msg = <p>Welcome to {name} React Site!! </p>
 ReactDOM.render(
     msg,
     document.getElementById("root"));
-
+/*
     let counter = 0 
     function show() {
         counter++;
@@ -20,4 +20,12 @@ ReactDOM.render(
         
     }
 setInterval(show,2000);
+*/
 
+///There are two types of Components
+//1-functional component  2- class component
+//here is functional component example 
+
+function HelloWorld() {
+    return <div>Willkommen auf {name} Seite.</div>
+}
